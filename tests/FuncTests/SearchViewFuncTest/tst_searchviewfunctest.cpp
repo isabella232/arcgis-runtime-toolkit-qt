39f4495bbeff7a173aa64e0027604a706a7701bd
Alessio Williams Gava <awilliams@esri.com>
2021-12-13 15:51:31 +0000

    inital set up searchview functional tests

---
+++ tests/FuncTests/SearchViewFuncTest/tst_searchviewfunctest.cpp
@@ -0,0 +1,19 @@
+// add necessary includes here
+#include "tst_searchviewfunctest.h"
+
+SearchViewFuncTest::SearchViewFuncTest()
+{
+
+}
+
+SearchViewFuncTest::~SearchViewFuncTest()
+{
+
+}
+
+void SearchViewFuncTest::test_case1()
+{
+
+}
+
+QTEST_MAIN(SearchViewFuncTest)
---
+++ tests/FuncTests/SearchViewFuncTest/tst_searchviewfunctest.h
@@ -0,0 +1,18 @@
+#ifndef TST_SEARCHVIEWFUNCTEST_H
+#define TST_SEARCHVIEWFUNCTEST_H
+
+#include <QtTest>
+
+class SearchViewFuncTest : public QObject
+{
+  Q_OBJECT
+
+public:
+  SearchViewFuncTest();
+  ~SearchViewFuncTest();
+
+private slots:
+  void test_case1();
+};
+
+#endif // TST_SEARCHVIEWFUNCTEST_H
